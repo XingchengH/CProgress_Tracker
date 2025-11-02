dbc7487e328c714a1b4065479fa9e3cb5bafb7b2
Xingcheng H <Huangxcxc@gmail.com>
2025-11-01 21:31:08 -0400

    removing frontend/.next folder

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__RSC_SERVER_MANIFEST =
-  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "KbejlJrp5yjxwM8KeW52lO+nrZsvT75S7V4WLyaenjI="\n}';
--- frontend/.next/dev/static/chunks/frontend_app_login_page_tsx_b9cd999e._.js
+++
@@ -1,5 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-  script: typeof document === 'object' ? document.currentScript : undefined,
-  chunks: ['static/chunks/frontend_5d5241ff._.js'],
-  source: 'dynamic',
-});
